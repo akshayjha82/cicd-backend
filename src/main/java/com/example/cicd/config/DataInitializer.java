e0cd5cb78946efe04e428511b27988d7c0518b97
Akshay Jha <akshay.jha@kiya.ai>
2026-09-02 11:31:13 +0530

    added a new product

--- src/main/java/com/example/cicd/config/DataInitializer.java
+++ src/main/java/com/example/cicd/config/DataInitializer.java
@@ -16,6 +16,8 @@ public class DataInitializer {
             repo.save(new Product(null, "Laptop", 50000));
             repo.save(new Product(null, "Asus", 78900));
             repo.save(new Product(null, "Phone", 30000));
+            repo.save(new Product(null, "Ipad", 37890));
+
         };
     }
 }
